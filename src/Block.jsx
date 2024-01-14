f04cce55bcd8983d76464b435caa8f207f3354ff
Levladik <vladislavtyshenko@yandex.ru>
2024-01-14 20:54:09 +0400

    feat: fix layout

--- src/Block.jsx
+++ src/Block.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP', 'CNY'];
+const defaultCurrencies = ['RUB', 'USD', 'EUR', 'GBP'];
 
 export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
   <div className="block">
@@ -13,12 +13,12 @@ export const Block = ({ value, currency, onChangeValue, onChangeCurrency }) => (
           {cur}
         </li>
       ))}
-      {/* <li>
+      <li>
         <svg height="50px" viewBox="0 0 50 50" width="50px">
           <rect fill="none" height="50" width="50" />
           <polygon points="47.25,15 45.164,12.914 25,33.078 4.836,12.914 2.75,15 25,37.25 " />
         </svg>
-      </li> */}
+      </li>
     </ul>
     <input
       onChange={(e) => onChangeValue(e.target.value)}
