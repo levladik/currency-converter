aacdfa562a02f4c0d66534431e6b131261feee62
Levladik <vladislavtyshenko@yandex.ru>
2024-01-14 20:43:10 +0400

    feat: add logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import Block  from './Block';
+import React, { useEffect, useRef, useState } from 'react';
+import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+
+  const ratesRef = useRef({});
+
+  useEffect(() => {
+    fetch('https://openexchangerates.org/api/latest.json?app_id=c00bca64619341808cbf42a460d40645')
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = json.rates;  
+        onChangeToPrice(1); 
+      })
+      .catch((err) => {
+        console.warn(err);
+        console.log('Failed to retrieve data');
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+
+    setToPrice(result.toFixed(3));
+    setFromPrice(value);    
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  };
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <Block />
-      <Block />
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice}/>
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToPrice}/>
     </div>
   );
 }
